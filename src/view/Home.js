60aa8febaf9ac17eb8b21e48b5976e4e9b49c714
Ania <an.pasieczna@gmail.com>
2023-02-11 00:40:49 +0100

    extracting team names

--- src/view/Home.js
+++ src/view/Home.js
@@ -1,21 +1,29 @@
 import React, { useEffect } from "react";
+import axios from "axios";
 import { useState } from "react";
 import { fetchData } from "../services/services";
 
 export const Home = () => {
-  const [data, setData] = useState("no data");
-  const getAllData = async () => {
-    const response = await fetchData();
-    //console.log("the response", response);
-    setData(response.data);
-  };
-  console.log(getAllData);
+  const [data, setData] = useState([]);
+
   useEffect(() => {
-    getAllData();
+    const API = "http://localhost:3007/schedules/";
+
+    axios
+      .get(API)
+      .then((response) => {
+        const responseItems = response.data;
+        console.log(responseItems);
+        setData(responseItems);
+      })
+      .catch((e) => {
+        console.error(e);
+      });
   }, []);
   console.log(data);
-
-  const getTeamNames = data.map((item) => item.sport_event.competitors);
-  console.log(getTeamNames);
+  const getNames = data.map((item) => item.sport_event.competitors);
+  console.log(getNames);
+  const names = getNames.map((item) => [item[0].name, item[1].name]);
+  console.log(names);
   return <div>Home</div>;
 };
