267840f510b7fcc2990a353665c538c9cc13c793
Ania <an.pasieczna@gmail.com>
2023-02-20 23:48:22 +0100

    matchTimeline cleaning

--- src/view/MatchTimeline/MatchTimeline.js
+++ src/view/MatchTimeline/MatchTimeline.js
@@ -10,17 +10,8 @@ import { fetchResponse } from "../../services/services";
 import { TimelineTable } from "../../components/TimelineTable/TimelineTable";
 export const MatchTimeline = () => {
   const global = useContext(GlobalState);
-  const [eventData, setEventData] = useState([]);
   const [timelineData, setTimelineData] = useState([]);
-  const getEventData = async () => {
-    try {
-      const eventResponse = await fetchResponse(global.globalEventApi);
 
-      setEventData(eventResponse.data);
-    } catch (e) {
-      console.log(e);
-    }
-  };
   const getTimelineData = async () => {
     try {
       const timelineResponse = await fetchResponse(global.globalTimelineApi);
@@ -29,28 +20,17 @@ export const MatchTimeline = () => {
       console.log(e);
     }
   };
-
   useEffect(() => {
-    getEventData();
     getTimelineData();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-  console.log(eventData);
-  // const homeTeam = eventData.competitors[0].name;
-
-  // const awayTeam = eventData.competitors[1].name;
-  // console.log(awayTeam);
   const actionType = timelineData.map((item) => item.type);
-
-  // console.log(actionType);
   const matchTime = timelineData.map((item) =>
     item.match_clock ? item.match_clock : "-"
   );
-  //console.log(matchTime);
   const team = timelineData.map((item) =>
     item.competitor ? item.competitor : "-"
   );
-  //console.log(team);
   return (
     <div className={styles.titleDiv}>
       <h2>The match step by step</h2>
