85604ab660d27ce0bc9f30e1143e426208bc5908
Ania <an.pasieczna@gmail.com>
2023-02-20 13:17:30 +0100

    timeline and timelinetable comp created compiled

--- src/components/TimelineTable/TimelineTable.js
+++ src/components/TimelineTable/TimelineTable.js
@@ -1,5 +1,27 @@
 import React from "react";
-
-export const TimelineTable = () => {
-  return <div>TimelineTable</div>;
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+export const TimelineTable = ({ team, matchTime, actionType }) => {
+  const createTeamCell = team.map((item) => {
+    return <div>{item}</div>;
+  });
+  const createMatchTimeCell = matchTime.map((item) => {
+    return <div>{item}</div>;
+  });
+  const createActionTypeCell = actionType.map((item) => {
+    return <div>{item}</div>;
+  });
+  return (
+    <Container>
+      <Row>
+        <Col>2 of 2</Col>
+      </Row>
+      <Row>
+        <Col>{createMatchTimeCell}</Col>
+        <Col>{createTeamCell}</Col>
+        <Col>{createActionTypeCell}</Col>
+      </Row>
+    </Container>
+  );
 };
