99bbdfbd7c1873cfb82669e4fa9a737db39e9661
Ania <an.pasieczna@gmail.com>
2023-02-21 08:53:08 +0100

    adding logo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 //Style
 import styles from "./App.module.scss";
+import player from "./media/player.png";
 //GlobalStore
 import { GlobalStore } from "./Store/GlobalStore";
 //Router
@@ -24,7 +25,12 @@ function App() {
     <GlobalStore>
       <div className={styles.App}>
         <header className={styles.AppHeader}>
-          <h1 className={styles.title}>EKSTRAKLASA MATCH RESULTS</h1>
+          <div className={styles.logoWrapper}>
+            <div className={styles.logoWrapper}>
+              <img className={styles.logo} src={player} alt='bookSketch'></img>
+            </div>
+            <h1 className={styles.title}>EKSTRAKLASA MATCH RESULTS</h1>
+          </div>
           <Navigation navElements={navElements} />
           <Routes>
             <Route path='/' element={<Home />} />
